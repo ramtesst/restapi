3cc71f30217c2398a6085ac50bbdffaa2cdf43ae
Rambabu Duddukuri <duddukurir@vmware.com>
2020-10-16 12:21:03 -0700

    Test Provider

--- restapi/main.tf
+++ restapi/main.tf
@@ -1,20 +1,4 @@
-provider "restapi" {
-  uri                  = "https://www.google.com"
-  debug                = true
-}
-
-provider "aws" {
-    region="us-east-1"
-}
-
-variable "instance_type" {
-  description = "AWS instance type"
-  default     = "t2.micro"
-}
-
-
-resource "aws_instance" "machine1" {
-    ami           = "ami-04b9e92b5572fa0d1"
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
+data "restapi" "weather" {
+  uri          = "https://google.com"
+  method       = "GET"
 }
